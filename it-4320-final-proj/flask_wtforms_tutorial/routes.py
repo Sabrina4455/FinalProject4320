d7c7db1a04e413c26de64d18fc9ae09cf7f3b623
jlf39 <89615721+jlf39@users.noreply.github.com>
2022-12-09 17:56:37 -0600

    cost matrix and seating chart

--- it-4320-final-proj/flask_wtforms_tutorial/routes.py
+++ it-4320-final-proj/flask_wtforms_tutorial/routes.py
@@ -21,16 +21,38 @@ def user_options():
 
 @app.route("/admin", methods=['GET', 'POST'])
 def admin():
-
+    #temp is the 2d array storing the seating chart
+    #sum1 is storing the total value of all the occupied seats
     form = AdminLoginForm()
     if request.method == 'POST' and form.validate_on_submit():
+        sum1=0
+        cm=get_cost_matrix()
+        row=12
+        col=4
+        temp=[[] for item in range(row)]
+        for item in range(row):
+            for items in range(col):
+                temp[item].append("O")
+        temp1=[]
+        f=open("test.txt",'r')
+        for item in f:
+            x=item.split()
+            y=x[1]
+            z=x[2]
+            z=int(z.replace(',',""))
+            y=int(y.replace(',',""))
+            temp2=[y,z]
+            temp1.append(temp2)
+        for item in temp1:
+            temp[item[0]][item[1]]="X"
+            sum1=sum1+(cm[item[0]][item[1]])
         username = request.form['username']
         password = request.form['password']
         f = open("passcodes.txt",'r')
         for item in f:
             x = item.split(',')
             if username == x[0].strip() and password == x[1].strip():
-                return render_template("adminopen.html", form=form, template="form-template")
+                return render_template("admin.html", form=form, template="form-template")
     return render_template("admin.html", form=form, template="form-template")
 
 @app.route("/reservations", methods=['GET', 'POST'])
@@ -49,19 +71,9 @@ def reservations():
         return render_template("reservations.html", form=form, template="form-template",message = 'Reservation succesfful, your code is: '+ y)
     return render_template("reservations.html", form=form, template="form-template")
 
-@app.route("/adminopen", methods=['GET', 'POST'])
-def adminopen():
-    """
-    form = AdminLoginForm()
-    if request.method == 'POST' and form.validate_on_submit():
-        first_name = request.form['first_name']
-        row = request.form['row']
-        seat = request.form['seat']
-        x = 15
-        y = ''.join(random.choices(string.ascii_uppercase + string.digits, k = x))
-        f = open('reservations.txt','a')
-        f.write(first_name+','+row+','+seat+','+y+'\n')
-        f.close()
-        """
-    return render_template("adminopen.html",template="form-template")
+
+
+def get_cost_matrix():
+    cost_matrix = [[100,75,50,100] for row in range(12)]
+    return cost_matrix
 
